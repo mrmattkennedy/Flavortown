34f22aa15cbe81e2784c77881f94c8244ba9c630
Kennedy <matthew.kennedy1@stryker.com>
2019-11-11 13:59:22 -0500

    Added working map functionality as well as what is needed for titles on marker info

--- flavortown_scraper.py
+++ flavortown_scraper.py
@@ -1,29 +1,67 @@
 import requests
 import copy
+import re
 from lxml import html
 from bs4 import BeautifulSoup
 
-page = requests.get('https://www.tvfoodmaps.com/T_TVShows.jsp')
-webpage = html.fromstring(page.content)
+#Need to get info on restaurants for markers, as well as show title
+#Do it as a 3d list, parse for title in beginning
+def get_addresses():
+    #Starting point: get the show list from here
+    page = requests.get('https://www.tvfoodmaps.com/T_TVShows.jsp')
+    webpage = html.fromstring(page.content)
 
-#Get a list of all teh shows
-shows = []
-for link in webpage.xpath('//a/@href'):
-    if link.startswith("/show") and not any(link in existing_link for existing_link in shows):
-        shows.append(link)
+    #Get a list of all the shows
+    shows = []
+    for link in webpage.xpath('//a/@href'):
+        if link.startswith("/show") and not any(link in existing_link for existing_link in shows):
+            shows.append(link)
 
-#Append the rest of the link to the shows
-shows = ["https://www.tvfoodmaps.com" + show + "/all" for show in shows]
+    #Append the rest of the link to the shows
+    show_names = [show[len('/show/'):].replace('-', ' ') for show in shows]
+    print(show_names)
+    shows = ["https://www.tvfoodmaps.com" + show + "/all" for show in shows]
 
-#Get all addresses listen in all shows
-addresses = []
-for show in shows:    
-    show_link = requests.get(show)
-    soup = BeautifulSoup(show_link.content.decode("utf-8", errors='ignore'),'html.parser')
-    restaurants = soup.findAll("div", {"class": "inner-results"})
-    temp = [str(restaurant.findAll("p", {"class": "searchResAddress"})) for restaurant in restaurants]
-    print(len(temp))
-    addresses.append(copy.deepcopy(temp))
+    #Get all addresses listed in all shows
+    addresses = []
+    address_beginner = '[<p class="searchResAddress">'
+    address_end = '</p>]'
+    name_beginner = '<h3 style="display:inline"><a href='
+    name_end = '</a> </h3>]'
+    phone_regex = "\(\w{3}\) \w{3}-\w{4}"
+    
+    for show in shows:
+        #Get a link to the show page
+        show_link = requests.get(show)
+        #Parse with beautifulsoup
+        soup = BeautifulSoup(show_link.content.decode("utf-8", errors='ignore'),'html.parser')
+        #Get a list of all restaurants
+        restaurants = soup.findAll("div", {"class": "inner-results"})
+        
+        #Get the addresses specifically, then clean them up
+        names = [str(restaurant.findAll("h3", {"style": "display:inline"})) for restaurant in restaurants]
+        names = [name[name.find('>', len(name_beginner)) + 1:len(name) - len(name_end)] for name in names]
+        
+        temp_addresses = [str(restaurant.findAll("p", {"class": "searchResAddress"})) for restaurant in restaurants]
+        temp_addresses = [address[len(address_beginner):len(address) - len(address_end)] for address in temp_addresses]
+        temp_addresses = [re.sub(phone_regex, '', address) for address in temp_addresses]
 
-print("Addresses is " + str(len(addresses)))
-#tags = [str(tag) for tag in tags]
+        #Combine lists, map them as lists in a list with list comprehension
+        temp = [list(info) for info in zip(temp_addresses, names)]
+        addresses.append(copy.deepcopy(temp))
+
+
+    #Sometimes there is no address, which means just remove it
+    for address_list in addresses:
+        for address in address_list:
+            if address[0].startswith(' ') and not address[0][1].isdigit():
+                address_list.remove(address)
+    
+    return addresses
+
+#temp = get_addresses()
+
+#print(len(temp))
+#print(len(temp[0]))
+#print(temp[3])
+#print(temp)
--- temp.py
+++ temp.py
@@ -1,10 +1,20 @@
 from gmplot import gmplot
+import flavortown_scraper
 
 # Place map
 gmap = gmplot.GoogleMapPlotter(37.09024, -95.712891, 4)
 
-hidden_gem_lat, hidden_gem_lon = 37.770776, -122.461689
-gmap.marker(hidden_gem_lat, hidden_gem_lon, 'cornflowerblue')
+#hidden_gem_lat, hidden_gem_lon = 37.770776, -122.461689
+#gmap.marker(hidden_gem_lat, hidden_gem_lon, 'cornflowerblue')
+address_list = flavortown_scraper.get_addresses()
+#for address in address_list[3]:
+ #   print(address)
+    
+for address in address_list[6]:
+    hidden_gem_lat, hidden_gem_lon = gmap.geocode(address[0])
+    gmap.marker(hidden_gem_lat, hidden_gem_lon, 'cornflowerblue', title=address[1])
+#print(address)
 
 # Draw
 gmap.draw("my_map.html")
+ 
