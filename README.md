f4c93f603f07f8c4202dda750a350ba529722fc3
Kennedy <matthew.kennedy1@stryker.com>
2019-11-15 15:05:21 -0500

    Added comments

--- README.md
+++ README.md
@@ -5,7 +5,7 @@ All the addresses, shows, restaurants, etc I got from https://www.tvfoodmaps.com
 I did some scraping off each show's page, and now there are over 6,000 markers on a single google map, which is why this website is so slow (also this is a [known issue](https://issuetracker.google.com/issues/35820227)
 
 ##Requirements
-[gmplot](https://pypi.org/project/gmplot/)
-I have attached a copy of my own gmplot file that has a few changes. The only change that is required is the change in pathing for *self.coloricon*.
+[gmplot](https://pypi.org/project/gmplot/) I have attached a copy of my own gmplot file that has a few changes. The only change that is required is the change in pathing for *self.coloricon*.
 [lxml](https://lxml.de/installation.html)
 [BeautifulSoup](https://pypi.org/project/beautifulsoup4/)
+[requests](https://pypi.org/project/requests/)
--- python/map_creator.py
+++ python/map_creator.py
@@ -4,27 +4,33 @@ import time
 from gmplot import gmplot
 import flavortown_scraper
 
+#This takes about 45 minutes because 6,000 geocode requests is really slow
 start_time = time.time()
 
-
-#key = [line.rstrip('\n') for line in open(key_location, 'r')][0]
+#Get the path to the api so I can make geocode GET requests
 key_path = sys.path[0] + "\\api.txt"
+#Get the first line in the file, which is the API key
 key = [line.rstrip('\n') for line in open(key_path, 'r')][0]
+
+#Points to where libraries are
 marker_path = sys.path[4]
 marker_path = os.path.join(marker_path, 'Lib\\site-packages\\')
+#Find gmplot
 for item in os.listdir(marker_path):
     if 'gmplot' in item:
         marker_path = os.path.join(marker_path, item + '\markers')
         break
 
 if not os.path.isdir(marker_path):
-    print("Can't find path to markers")
+    raise Exception("Can't find path to markers, is gmplot installed?")
 
+#Get every other marker, and take the .png off of the end.
 every_other_marker = ['#' + color[:-4] for color in os.listdir(marker_path)[::2]]
 
 # Place map
 gmap = gmplot.GoogleMapPlotter(37.09024, -95.712891, 4, apikey=key)
 address_list, shows = flavortown_scraper.get_addresses()
+
 
 color_index = 0;
 for show_index in range(len(address_list)):
@@ -34,13 +40,9 @@ for show_index in range(len(address_list)):
             show_title = "Name: " + address[1] + "<br>Show: " + shows[show_index]
             gmap.marker(hidden_gem_lat, hidden_gem_lon, color=every_other_marker[color_index], title=show_title)
         except Exception:
-            print(address)
             continue
     color_index+=1
-    
-for color in every_other_marker:
-    print(color)
 
 # Draw
-gmap.draw("my_map.html")
+gmap.draw("flavortown.html")
 print("--- %s seconds ---" % (time.time() - start_time))
